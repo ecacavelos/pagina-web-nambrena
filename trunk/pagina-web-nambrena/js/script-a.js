c3ec7ff28d19151049accac323b6351c856bd224
david.eaj@gmail.com <david.eaj@gmail.com@d395f487-b3ec-3e8f-6f83-57ce2d24d198>
2012-02-08 19:22:13 +0000

    Pagina web nambrena Incluye las paginas: - Pagina Frontal - Pagina de Carteles:   Front Light y Front Light Sobre Pared   Back Light
    Incluye versiones tentativas de las animaciones y transiciones de pagina.

---
+++ trunk/pagina-web-nambrena/js/script-a.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$("body").css("display", "none");
+	$("body").fadeIn(750);
+	
+	$("a.transition-out").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        //$("body").fadeOut(750, redirectPage);
+		$("#content-lower").animate({
+			/*opacity: 0,*/
+			left: '-=1920px'
+			/*height: 'toggle'*/
+		}, 500, redirectPage1
+		);
+    });
+ 
+    function redirectPage1() {
+        $("body").fadeOut(500, redirectPage2);
+    }
+	
+    function redirectPage2() {
+        window.location = linkLocation;
+    }	
+	
+	function displayPage() {
+		$("body").fadeIn(750);
+	}
+
+	window.addEventListener("load", displayPage, false);
+	window.addEventListener("unload", displayPage, false);
+	
+});
