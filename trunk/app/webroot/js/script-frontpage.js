ace7aac22c5753906f3ad45cb3f486ce6d82c4d8
estebancacavelos@gmail.com <estebancacavelos@gmail.com@d395f487-b3ec-3e8f-6f83-57ce2d24d198>
2012-02-13 15:09:44 +0000

    Se agregaron los archivos css  que faltaban a la estructura del cake y el archivo script-frontpage.js
    La pagina principal (default.ctp) esta practicamente terminada. Con los links direccionados correctamente. 
    
    Se actualizaron los archivos css y js de la version HTML pura de david.eaj@gmail.com.

--- trunk/app/webroot/js/script-frontpage.js
+++ trunk/app/webroot/js/script-frontpage.js
@@ -1,38 +1,26 @@
 $(document).ready(function() {
 	$("body").css("display", "none");
-	$("body").fadeIn(750, redirectEnter1);
+	$("body").fadeIn(750);
+	$("#content-lower-menu").animate({left: '0px'}, 0);
 	
 	$("a.transition-out").click(function(event){
         event.preventDefault();
         linkLocation = this.href;
+        //$("body").fadeOut(750, redirectPage);
 		$("#content-lower-menu").animate({
+			/*opacity: 0,*/
 			left: '-=1920px'
-		}, 500, redirectPage1
-		);
-    });
-	
-	$("a.transition-back").click(function(event){
-        event.preventDefault();
-        linkLocation = this.href;
-		$(this).animate({
-			opacity: '0%'
+			/*height: 'toggle'*/
 		}, 500, redirectPage1
 		);
     });
  
     function redirectPage1() {
-        $("body").fadeOut(750, redirectPage2);
+        $("body").fadeOut(500, redirectPage2);
     }
 	
     function redirectPage2() {
         window.location = linkLocation;
-    }
-	
-	function redirectEnter1() {
-		$("#content-lower-menu").animate({
-			left: '0px'
-		}, 500
-		);
     }	
 	
 	function displayPage() {
@@ -41,4 +29,5 @@ $(document).ready(function() {
 
 	window.addEventListener("load", displayPage, false);
 	window.addEventListener("unload", displayPage, false);
+	
 });
