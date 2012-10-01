3de5a2069a97d09ebe6eec9de0269a52b9458279
estebancacavelos@gmail.com <estebancacavelos@gmail.com@d395f487-b3ec-3e8f-6f83-57ce2d24d198>
2012-10-01 15:35:19 +0000

    Se aplica la formula en formula_CARTELES.xlsx. Se modifico el archivo de configuracion, corrigiendo un error de concepto que habia.

--- trunk/app/Config/nambrena_config.php
+++ trunk/app/Config/nambrena_config.php
@@ -8,9 +8,9 @@ $config['Cartel']['instalacion_factor'] = 0.98;
 $config['Cartel']['frontLight'] = 5000;
 $config['Cartel']['backLight'] = 10000;
 $config['Cartel']['adhesivo'] = 15000;
-$config['Cartel']['frontLight_LONA'] = 2000;
-$config['Cartel']['backLight_LONA'] = 3000;
-$config['Cartel']['adhesivo_LONA'] = 4000;
+$config['Cartel']['frontLight_MANTENIMIENTO'] = 2000;
+$config['Cartel']['backLight_MANTENIMIENTO'] = 3000;
+$config['Cartel']['adhesivo_MANTENIMIENTO'] = 4000;
 
 $config['Impresion']['frontLight'] = 7000;
 $config['Impresion']['backLight'] = 9000;
